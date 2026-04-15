4e1e3e9d709ad92ccf69a89fd21516ea8fdc40ba
VASADUNESH <duneshvasa@gmail.com>
2026-04-15 18:35:01 +0530

    Add full Vite React project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+    return (
+        <div>
+            <h1 class="text-3xl  underline">Welcome to my dunesh show case!</h1>
+        </div>
+    );
+};          
+export default App;
