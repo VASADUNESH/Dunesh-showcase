7e97a7eb0a30fe9839f02140fcbf2a5b42d7a9cc
VASADUNESH <duneshvasa@gmail.com>
2026-04-15 19:39:40 +0530

    feat: setup vite config with path aliases and base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: "/Dunesh-showcase/",
   plugins: [react()],
+  base: '/Dunesh-showcase/',
+  resolve: {
+    alias: {
+      "#components": resolve(__dirname, "src/components"),
+      "#constants": resolve(__dirname, "src/constants"),
+      "#store": resolve(__dirname, "src/store"),
+      "#hoc": resolve(__dirname, "src/hoc"),
+      "#windows": resolve(__dirname, "src/windows"),
+    },
+  },
 })
